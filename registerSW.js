7081adcb989107059539cbc620ec510a81ba3ca6
project-sy789 <jamesme_22@hotmail.com>
2026-03-22 19:25:20 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/QR-code-generator/sw.js', { scope: '/QR-code-generator/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
